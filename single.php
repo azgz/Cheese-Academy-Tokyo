b4243400a78c1aa755b5afbe56249ef80acd0d55
azgz <azmax.koichi+gz@gmail.com>
2015-07-16 17:24:19 +0900

    ニュース詳細ページにWP テンプレートタグを組み込み

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
             <div class="inner clearfix">
                 <div class="section-heading-wrap">
                     <h2 class="section-title white">NEWS</h2>
-                    <p class="section-note">2015.04.03</p>
+                    <p class="section-note"><?php the_time('Y.m.d'); ?></p>
                 </div>
             </div>
             </div>
@@ -14,8 +14,8 @@
                 <ul class="news_list clearfix">
                     <li> 
                     <dl>
-	                    <dt class="news-date clearfix"><span class="news_tags">タイトル</span></dt>
-	                    <dd class="news-title">詳細情報・・・・</dd>
+	                    <dt class="news-date clearfix"><span class="news_tags"><?php the_title(); ?></span></dt>
+	                    <dd class="news-title"><?php the_content(); ?></dd>
 	                    </dl>
                     </li>
                 </ul>
