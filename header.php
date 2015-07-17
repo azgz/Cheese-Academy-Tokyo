4a06c5b5a7b03dc4a5542257e497b8a1e7b9970b
azgz <azmax.koichi+gz@gmail.com>
2015-07-17 09:30:53 +0900

    Topと個別記事ページでheaderが違うのを出しわけ処理を追加

--- header.php
+++ header.php
@@ -36,6 +36,7 @@
     </header>
 
     <!--　main_visual   -->
+<?php if(is_home() || is_front_page()): ?>
     <section id="main_visual" class="slidesWrap">
         <div class="inner">
             <h2 class="catch">授業後払いのチーズ職人養成学校<br />『チーズアカデミーTOKYO』はじまります。</h2>
@@ -64,4 +65,19 @@
         </div>
         
     </section>
-    
+<?php else : ?>
+    <section class="nav_lower">
+        <div class="inner">
+            <nav class="global_Nav">
+                <ul class="nav_wrap clearfix">
+                    <li class="nav_item"><a href="#">HOME<br />-ホーム-</a></li>
+                    <li class="nav_item"><a href="#">NEWS<br />-お知らせ・更新情報-</a></li>
+                    <li class="nav_item"><a href="#">FEATURE<br />-特徴-</a></li>
+                    <li class="nav_item"><a href="#">COURCE<br />-コース紹介-</a></li>
+                    <li class="nav_item none"><a href="#">GALLERY<br />-ギャラリー-</a></li>
+                    <li class="nav_item last"><a href="#">ENTRY<br />-説明会に申し込む-</a></li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+<?php endif; ?>
